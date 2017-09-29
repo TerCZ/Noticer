7b7ac25761701ad21ec06b6004c276f7401b7470
TerCZ <t.chuzhe@qq.com>
2017-09-29 11:18:08 +0800

    add test entry point to main procedure

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
 """Combine crawling and mailing part together."""
+import sys
 
 from crawling import run_spider
-from mailing import send_mails
+from mailing import send_mails, send_mails_test
 
 
 def main():
     """Main procedure."""
     run_spider()
-    send_mails()
+    if len(sys.argv) > 1:
+        send_mails_test()
+    else:
+        send_mails()
 
 
 if __name__ == '__main__':
